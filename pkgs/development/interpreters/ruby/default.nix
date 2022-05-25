81b3119c9e42d606d518c17949baf0e2955362d3
Jeff Hykin <jeff.hykin@gmail.com>
2022-05-25 12:49:45 -0500

    -

--- pkgs/development/interpreters/ruby/default.nix
+++ pkgs/development/interpreters/ruby/default.nix
@@ -254,7 +254,7 @@ in {
   ruby_2_6 = generic {
     version = rubyVersion "2" "6" "9" "";
     sha256 = {
-      src = lib.fakeSha256;
+      src = "1fzgn1w62y71fgan6vxfcaxwql74zb5a841p2nr9xgv4mixawyzb";
       git = "1v7x5zj31zjjy73ak0m3avqxyc0kcpykk926k853xgk85vq3y8bk";
     };
   };
